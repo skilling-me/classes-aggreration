4dbcc58e731461f25a4a9a87fa77ef851519f7d4
Nataly Tkachuk <techgirlua@gmail.com>
2015-09-09 16:49:26 +0300

    Add url to resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909132700) do
+ActiveRecord::Schema.define(version: 20150909134658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150909132700) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "url"
   end
 
   add_foreign_key "categorisations", "categories"
