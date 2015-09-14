72d97f75408c03cc97bf6b8111c9d11842a6351e
Nataly Tkachuk <techgirlua@gmail.com>
2015-09-14 18:29:32 +0300

    Configure devise for email authentication

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,6 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :new, :create]
 
   def index
     if params[:search]
